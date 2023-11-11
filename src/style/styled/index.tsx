0a1e28d867a062016be80616d51efd1b379acfee
kim-elijah-sol <kimelijahsol@gmail.com>
2023-11-11 12:45:47 +0900

    style: S dot 컨벤션으로 리팩토링

---
+++ src/style/styled/index.tsx
@@ -0,0 +1,139 @@
+import palette from 'style/palette'
+import { styled, css } from 'styled-components'
+
+const ShadowTop = styled.div`
+  height: 50px;
+  background-color: ${palette.gray_100};
+  box-shadow: 0px 2px 5px 0px rgba(0, 0, 0, 0.1);
+`
+
+const S = {
+  Aside: styled.aside`
+    width: 280px;
+    height: 100%;
+    border-right: 1px solid #c4c4c4;
+  `,
+  AsideTop: styled(ShadowTop)`
+    display: flex;
+    gap: 10px;
+    padding: 10px;
+  `,
+  Button: styled.button`
+    background-color: ${palette.gray_100};
+    width: 100%;
+    height: 30px;
+    text-align: center;
+    border-radius: 5px;
+    border: 1px solid ${palette.gray_90};
+    font-size: 12px;
+    transition: 0.21s;
+
+    &:hover {
+      background-color: ${palette.gray_90};
+    }
+  `,
+  Container: styled.div`
+    width: 100%;
+    height: 100vh;
+    display: flex;
+    background-color: ${palette.gray_97};
+  `,
+  IconButton: styled.button`
+    transition: 0.21s;
+    border-radius: 6px;
+
+    &:hover {
+      background-color: #ccc;
+    }
+  `,
+  ResourceItem: styled.li<{ active?: boolean }>`
+    height: 90px;
+    background-color: ${palette.gray_100};
+    padding: 10px;
+    border-radius: 10px;
+    padding: 12px;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    cursor: pointer;
+    transition: 0.21s;
+    outline: 1px solid transparent;
+
+    &:hover {
+      outline-color: #ccc;
+    }
+
+    ${(props) =>
+      props.active &&
+      css`
+        outline-color: ${palette.system_blue} !important;
+      `}
+  `,
+  ResourceItemTop: styled.p`
+    font-size: 14px;
+    width: 100%;
+    text-overflow: ellipsis;
+    overflow: hidden;
+    word-break: break-word;
+    display: -webkit-box;
+    -webkit-line-clamp: 2;
+    -webkit-box-orient: vertical;
+  `,
+  ResourceItemBottom: styled.div`
+    display: flex;
+    justify-content: flex-end;
+    gap: 8px;
+  `,
+  ResourceList: styled.ul`
+    list-style: none;
+    display: flex;
+    flex-direction: column;
+    gap: 10px;
+    padding: 10px;
+    max-height: calc(100% - 50px);
+    overflow-y: auto;
+
+    &::-webkit-scrollbar {
+      display: none;
+    }
+  `,
+  Viewer: styled.main`
+    width: calc(100% - 280px);
+    height: 100%;
+    background-color: ${palette.gray_100};
+  `,
+  ViewerTop: styled(ShadowTop)`
+    padding: 16px 15px 15px 17px;
+    background-color: ${palette.gray_100};
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  `,
+  ViewerTitle: styled.p`
+    font-size: 14px;
+    width: calc(100% - 29px);
+
+    text-overflow: ellipsis;
+    overflow: hidden;
+    word-break: break-all;
+    white-space: nowrap;
+  `,
+  ViewerWebView: styled.iframe`
+    width: 100%;
+    height: calc(100% - 50px);
+  `,
+  ViewerImageContainer: styled.div`
+    width: 100%;
+    height: calc(100% - 50px);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    > img {
+      max-width: 100%;
+      max-height: 100%;
+    }
+  `,
+} as const
+
+export default S
