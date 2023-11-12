e6cde726c9020e084658a743aa2d3116b4487650
kim-elijah-sol <kimelijahsol@gmail.com>
2023-11-12 22:10:04 +0900

    fix: ShadowTop z-index 추가

--- src/style/styled/index.tsx
+++ src/style/styled/index.tsx
@@ -5,6 +5,8 @@ const ShadowTop = styled.div`
   height: 50px;
   background-color: ${palette.gray_100};
   box-shadow: 0px 2px 5px 0px rgba(0, 0, 0, 0.1);
+  position: relative;
+  z-index: 2;
 `
 
 const Input = styled.input`
