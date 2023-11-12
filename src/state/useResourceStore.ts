cc767653ac809106bb7a98040115bfaeac2af7b3
kim-elijah-sol <kimelijahsol@gmail.com>
2023-11-12 15:33:12 +0900

    feat: resource 상태 설계

---
+++ src/state/useResourceStore.ts
@@ -0,0 +1,67 @@
+import { create } from 'zustand'
+
+export type UrlResource = {
+  type: 'url'
+  url: string
+}
+
+export type ImageResource = {
+  type: 'image'
+  src: string
+  fileName: string
+}
+
+export type Resource = { id: number } & (UrlResource | ImageResource)
+
+type ResourceStore = {
+  resources: Resource[]
+  addResource: (urlResource: UrlResource | ImageResource) => void
+  editResource: (
+    id: number,
+    urlResource: Omit<UrlResource, 'type'> | Omit<ImageResource, 'type'>
+  ) => void
+  removeResource: (id: number) => void
+  selctedResourceId: number | null
+  setSelectedResourceId: (selctedResourceId: number | null) => void
+}
+
+const useResourceStore = create<ResourceStore>((set) => ({
+  resources: [
+    {
+      id: 1,
+      type: 'url',
+      url: 'https://www.robinwieruch.de/react-libraries/',
+    },
+    {
+      id: 2,
+      type: 'url',
+      url: 'https://typed.do/blog-kr/how-to-make-good-usability-product/',
+    },
+  ],
+  addResource: (resource) =>
+    set((state) => {
+      const id =
+        Math.max(...state.resources.map((resource) => resource.id), 0) + 1
+
+      return {
+        resources: state.resources.concat({ ...resource, id }),
+      }
+    }),
+  editResource: (id, newResource) =>
+    set((state) => ({
+      resources: state.resources.map((resource) =>
+        resource.id === id ? { ...resource, ...newResource } : resource
+      ),
+    })),
+  removeResource: (id) =>
+    set((state) => ({
+      resources: state.resources.filter((resource) => resource.id !== id),
+    })),
+  selctedResourceId: null,
+  setSelectedResourceId: (selctedResourceId) =>
+    set(() => ({
+      selctedResourceId,
+    })),
+}))
+
+export default useResourceStore
